f94fcf680de4129a9d6405c05b14cb9e6d190771
Masamichi-Someya <sometan91@gmail.com>
2023-04-22 23:57:59 +0900

    昨年駒場祭のを入れただけ　

---
+++ event/23_MayFesta/utatane.js
@@ -0,0 +1,17 @@
+function selectApps(){
+  link_table = new Array(	// リンク先の登録
+    "communication.html#witness",//人物推測ゲーム
+    "communication.html#poem",//ことばをかたどる
+    "communication.html#dialog",//対話リミットゲーム
+    "language_and_media.html#shopping",//買い物のものさし
+    "language_and_media.html#idioms",//らくわく慣用句
+    "language_and_media.html#language",//みるみるオノマトペ
+    "learning_and_shikake.html#shikake",//「しかけ」をつくる
+    "science_cognition.html#decision",//意思決定解析機
+    "science_cognition.html#expert"//「専門家」を再考する
+  );
+  
+  let t_rnd = Math.floor(Math.random()*link_table.length);
+  let link_url = link_table[t_rnd];
+  window.open(link_url);
+}
