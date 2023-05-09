2ca6c064d45fc2f1874e595c348a3a22a1006cc6
Masamichi-Someya <sometan91@gmail.com>
2023-05-10 02:05:40 +0900

    ver2

--- event/23_MayFesta/utatane.js
+++ event/23_MayFesta/utatane.js
@@ -1,14 +1,14 @@
 function selectApps(){
   link_table = new Array(	// リンク先の登録
-    "communication.html#witness",//人物推測ゲーム
-    "communication.html#poem",//ことばをかたどる
-    "communication.html#dialog",//対話リミットゲーム
-    "language_and_media.html#shopping",//買い物のものさし
-    "language_and_media.html#idioms",//らくわく慣用句
-    "language_and_media.html#language",//みるみるオノマトペ
-    "learning_and_shikake.html#shikake",//「しかけ」をつくる
-    "science_cognition.html#decision",//意思決定解析機
-    "science_cognition.html#expert"//「専門家」を再考する
+    "witness.html",//「人物像」を見つめる
+    "color.html",//科学のパッチワーク
+    "technology.html",//技術をさぐる
+    "objects.html",//ねじねじモノづくり
+    "standard.html",//「当たり前」は誰のもの？
+    "objectivity.html",//わたしの「客観」、みんなの「客観」
+    "wording.html",//言葉の裏側
+    "expression.html",//思ったことが言えない
+    "poem.html"//ことばをかたどる
   );
   
   let t_rnd = Math.floor(Math.random()*link_table.length);
